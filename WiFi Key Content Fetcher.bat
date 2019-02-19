3774b10a082041ef090e46a1814c8689dbcee9f8
Falcon <37326776+BlackFalcons@users.noreply.github.com>
2019-02-19 15:02:44 +0100

    Prevent dry coding

--- WiFi Key Content Fetcher.bat
+++ WiFi Key Content Fetcher.bat
@@ -1,23 +1,23 @@
 @echo off
-	:: Fetch network names
-	echo A list of currently known network names (SSID) &echo.
+    :: Fetch network names
+    echo A list of currently known network names (SSID) &echo.
     netsh wlan show profile | find "All User Profile"
     echo.
 
     :: Userinput variable SSID name
-	set /p input="Type the network SSID/name you wish to fetch the password from: "
+    set /p input="Type the network SSID/name you wish to fetch the password from: "
+    set networkContent=netsh wlan show profile %input% key=clear
+    set sepLine=echo ------------------------------------------------------------------
 
-	:: Save network information in file
-	netsh wlan show profile %input% key=clear | find "Key Content" > WiFi_Key_Content.txt
+    :: Save network information in file
+    %networkContent% | find "Key Content"> WiFi_Key_Content.txt
+    cls
 
-	:: Clears the command prompt
-	cls
+    :: Inform about network encryption key location
+    echo The password to the network is after the colon of the Key Content & echo.
+    %sepLine%
 
-	:: Inform about network encryption key location
-	echo The password to the network is after the colon of the Key Content & echo.
-	echo ------------------------------------------------------------------
-
-	:: Print network information on command propmpt
-	netsh wlan show profile %input% key=clear | find "Key Content"
-	echo ------------------------------------------------------------------ & echo.
+    :: Print network information on command propmpt
+    %networkContent% |  find "Key Content"
+    %sepLine% & echo.
 pause
